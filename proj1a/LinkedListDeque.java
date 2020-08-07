b8ea8c93e90d7f61f768740cde576bdb96ca4048
Lawrence Lee <test@gmail.com>
2020-08-07 11:05:28 +0800

    fixed return problem

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,8 +1,8 @@
 public class LinkedListDeque<genType> {
 
-    private static class genNode {
+    private class genNode {
         public genNode prev;
-        public genTypeT item;
+        public genType item;
         public genNode next;
 
         public genNode(genNode p, genType i, genNode n) {
@@ -12,13 +12,13 @@ public class LinkedListDeque<genType> {
         }
     }
 
-    private genNode<genType> sentinel;
+    private genNode sentinel;
     private int size;
 
 
     public LinkedListDeque() {
         size = 0;
-        sentinel = new genNode<genType>(null, null, null);
+        sentinel = new genNode(null, null, null);
         sentinel.prev = sentinel;
         sentinel.next = sentinel;
 
@@ -26,26 +26,26 @@ public class LinkedListDeque<genType> {
 
     public LinkedListDeque(genType item) {
         size = 1;
-        sentinel = new genNode<genType>(null, null, null);
-        sentinel.next= new genNode(sentinel,item,sentinel);
-        sentinel.prev= sentinel.next;
+        sentinel = new genNode(null, null, null);
+        sentinel.next = new genNode(sentinel, item, sentinel);
+        sentinel.prev = sentinel.next;
 
 
     }
 
     public void addFirst(genType item) {
         size += 1;
-        genNode tempPointer=sentinel.next;
-        sentinel.next = new genNode(sentinel,item,tempPointer);
-        tempPointer.prev=sentinel.next;
+        genNode tempPointer = sentinel.next;
+        sentinel.next = new genNode(sentinel, item, tempPointer);
+        tempPointer.prev = sentinel.next;
 
     }
 
     public void addLast(genType item) {
         size += 1;
         genNode tempPointer = sentinel.prev;
-        tempPointer.next=new genNode(tempPointer,item,sentinel);
-        sentinel.prev=tempPointer.next;
+        tempPointer.next = new genNode(tempPointer, item, sentinel);
+        sentinel.prev = tempPointer.next;
 
 
     }
@@ -63,57 +63,56 @@ public class LinkedListDeque<genType> {
 
     public void printDeque() {
         genNode tempPointer = sentinel.next;
-        if (tempPointer!=sentinel){
+        if (tempPointer != sentinel) {
             System.out.println(tempPointer.item);
-            tempPointer=tempPointer.next;
+            tempPointer = tempPointer.next;
 
         }
     }
 
     public genType removeFirst() {
-        if (size==0){
+        if (size == 0) {
             return null;
         }
         size -= 1;
-        genNode tempPointer =sentinel.next.next;
-        sentinel.next=tempPointer;
-        tempPointer.prev=sentinel;
+        genType tempItem = sentinel.next.item;
+        genNode tempPointer = sentinel.next.next;
+        sentinel.next = tempPointer;
+        tempPointer.prev = sentinel;
+        return tempItem;
 
     }
 
     public genType removeLast() {
-        if (size==0){
+        if (size == 0) {
             return null;
         }
         size -= 1;
+        genType tempItem = sentinel.prev.item;
         genNode tempPointer = sentinel.prev.prev;
-        tempPointer.next=sentinel;
-        sentinel.prev=tempPointer;
+        tempPointer.next = sentinel;
+        sentinel.prev = tempPointer;
+        return tempItem;
+
 
     }
 
     public genType get(int index) {
-        if (index>=size || index <0){
+        if (index >= size || index < 0) {
             return null;
         }
         genNode tempPointer = sentinel.next;
-        int tempIndex=index;
-        while(tempIndex>0){
-            tempPointer=tempPointer.next;
-            tempIndex-=1;
+        int tempIndex = index;
+        while (tempIndex > 0) {
+            tempPointer = tempPointer.next;
+            tempIndex -= 1;
         }
         return tempPointer.item;
     }
 
     public genType getRecursive(int index) {
-        if (index>=size ||index<0){
-            return null;
-        }
-        else if (index>0){
-            return this.next.getRecursive(index-1);
-        }
-        else{
-            return this.item;
-        }
+        return null;
     }
+
+
 }
